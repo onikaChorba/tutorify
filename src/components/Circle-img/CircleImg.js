3ebe2cc1da33c828e3d8b261fe8a7f3a00c20d76
Onika Chorba <onikachorba@MacBook-Pro-Dream-2.local>
2022-10-16 19:42:16 +0300

    add img and icon in component CircleImg

---
+++ src/components/Circle-img/CircleImg.js
@@ -0,0 +1,22 @@
+import './CircleImg.scss';
+import mainImg from '../../img/Circle-img.png';
+import icon1 from '../../img/circleIcon1.png';
+import icon2 from '../../img/circleIcon2.png';
+import icon3 from '../../img/circleIcon3.png';
+import icon4 from '../../img/circleIcon4.png';
+import icon5 from '../../img/circleIcon5.png';
+function CircleImg() {
+  return (
+    <div className="circle-img">
+      <img className='circle-img__img' src={mainImg} />
+      <div color='circle-img__circle'></div>
+      <div color='circle-img__line'></div>
+      <img className='circle-icon' src={icon1} />
+      <img className='circle-icon' src={icon2} />
+      <img className='circle-icon' src={icon3} />
+      <img className='circle-icon' src={icon4} />
+      <img className='circle-icon' src={icon5} />
+    </div>
+  );
+}
+export default CircleImg;
