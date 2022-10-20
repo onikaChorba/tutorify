cd12f0e7c4c1bee4a581917c02db6db576930ad2
Onika Chorba <onikachorba@MacBook-Pro-Dream-2.local>
2022-10-20 20:43:42 +0300

    add img in willget section and add style

--- src/Pages/Front-page/Front-page-data.js
+++ src/Pages/Front-page/Front-page-data.js
@@ -5,13 +5,15 @@ const data = [
     linkText: 'read more'
   },
   {
-    title: '1-on-1 lessons in more than 150 languages',
-    text: 'Learn from certified teachers with proven experience...',
+    title: 'Practice for free with the italki community',
+    text: 'Develop your language skills by building connections with others...',
     linkText: 'read more'
   },
   {
-    title: '1-on-1 lessons in more than 150 languages',
+    title: 'Take learning beyond the classroom',
     text: 'Learn from certified teachers with proven experience...',
     linkText: 'read more'
   },
 ]
+
+export default data;
