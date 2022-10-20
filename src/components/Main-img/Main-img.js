ffa78777878c934c4b6df1196e5a67efd8bc69fc
Onika Chorba <onikachorba@MacBook-Pro-Dream-2.local>
2022-10-20 21:35:59 +0300

    add array in main img

--- src/components/Main-img/Main-img.js
+++ src/components/Main-img/Main-img.js
@@ -1,18 +1,18 @@
 import './Main-img.scss';
 import heroImg from '../../img/hero-img.png';
-import flet1 from '../../img/flet1.png';
-import flet2 from '../../img/flet2.png';
-import flet3 from '../../img/flet3.png';
 import UserOnline from '../User-online/User-online';
+
+const flet = [
+  { src: './img/flet1.png', className: 'main-flet__1' },
+  { src: './img/flet2.png', className: 'main-flet__2' },
+  { src: './img/flet3.png', className: 'main-flet__3' }
+]
 
 function MainImg() {
   return (<div className='mainImg'>
     <div className='bg-img'></div>
     <img className='main-img' src={heroImg} alt="men" />
-
-    <img className='main-flet__1' src={flet1} alt="flet" />
-    <img className='main-flet__2' src={flet2} alt="flet" />
-    <img className='main-flet__3' src={flet3} alt="flet" />
+    {flet.map((index, key) => <img key={key} src={index.src} title={index.title} alt='flet' className={index.className} />)}
     <UserOnline />
   </div>
   );
