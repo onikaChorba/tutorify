ecfa3d7a80304c0c1795cfe33a7c0b6f305b248d
Onika Chorba <onikachorba@MacBook-Pro-Dream-2.local>
2022-11-08 21:14:31 +0200

    add function in showMoreFlet in component FletBox

--- src/Pages/FrontPage/components/FletBox/FletBox.jsx
+++ src/Pages/FrontPage/components/FletBox/FletBox.jsx
@@ -1,16 +1,30 @@
 import "./FletBox.scss";
+
 import arrow from "@/assets/img/arrow.png";
+import { useState } from "react";
+
 const fletIcon = [
-  { src: "./img/flet-icon1.png", title: "icon1", className: "flet-icon" },
-  { src: "./img/flet-icon2.png", title: "icon2", className: "flet-icon" },
-  { src: "./img/flet-icon3.png", title: "icon3", className: "flet-icon" },
-  { src: "./img/flet-icon4.png", title: "icon4", className: "flet-icon" },
-  { src: "./img/flet-icon5.png", title: "icon5", className: "flet-icon" },
-  { src: "./img/flet-icon6.png", title: "icon6", className: "flet-icon" },
-  { src: "./img/flet-icon7.png", title: "icon7", className: "flet-icon" },
+  { src: "./img/flet-icon1.png" },
+  { src: "./img/flet-icon2.png" },
+  { src: "./img/flet-icon3.png" },
+  { src: "./img/flet-icon4.png" },
+  { src: "./img/flet-icon5.png" },
+  { src: "./img/flet-icon6.png" },
+  { src: "./img/flet-icon7.png" },
+  { src: "./img/flet-icon1.png" },
+  { src: "./img/flet-icon2.png" },
+  { src: "./img/flet-icon3.png" },
 ];
 
 function FletBox() {
+  const fletIconRow = 3;
+  const [next, setNext] = useState(fletIconRow);
+  const handleMoreFlet = () => {
+    if (next < fletIcon.length) {
+      setNext(next + fletIconRow);
+    }
+  };
+
   return (
     <div className="fletBox">
       <p className="fletBox__text">
@@ -18,16 +32,18 @@ function FletBox() {
         faucibus ridiculus placerat ipsum. Volutpat eget ut vitae amet
         ullamcorper et, ante venenatis.
       </p>
-      {fletIcon.map((index, key) => (
+      {fletIcon.slice(0, next).map((index, key) => (
         <img
           key={key}
           src={index.src}
-          title={index.title}
           alt="flet"
-          className={index.className}
+          style={index.style}
+          className="flet-icon"
         />
       ))}
-      <button className="fletBox__button"> View All</button>
+      <button className="fletBox__button" onClick={handleMoreFlet}>
+        View All
+      </button>
       <img src={arrow} alt="arrow" />
     </div>
   );
