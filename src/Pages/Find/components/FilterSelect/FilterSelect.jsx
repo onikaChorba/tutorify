2e6f83fea883a700e45749eb731b171e9c324e56
Onika Chorba <onikachorba@MacBook-Pro-Dream-2.local>
2022-12-18 10:49:07 +0200

    filter select without show component

--- src/Pages/Find/components/FilterSelect/FilterSelect.jsx
+++ src/Pages/Find/components/FilterSelect/FilterSelect.jsx
@@ -1,21 +1,33 @@
 import "./FilterSelect.scss";
 import React from "react";
 
-export default function FilterSelect({}) {
-  return (
-    <div className="filterSelect">
-      <select>
-        <option value="all" className="selectOption">
-          All Lessons
-        </option>
+class FilterSelect extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "all" };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
 
-        <option value="high" className="selectOption">
-          Popular Lessons
-        </option>
-        <option value="low" className="selectOption">
-          Unpopular Lessons
-        </option>
-      </select>
-    </div>
-  );
+  render() {
+    return (
+      <div className="filterSelect">
+        <select value={this.state.value} onChange={this.handleChange}>
+          <option value="all" className="selectOption">
+            All Lessons
+          </option>
+          <option value="high" className="selectOption">
+            Popular Lessons
+          </option>
+          <option value="low" className="selectOption">
+            Unpopular Lessons
+          </option>
+        </select>
+      </div>
+    );
+  }
 }
+
+export default FilterSelect;
