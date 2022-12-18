a07ba5dc03f58324265684e525fbcf234242b0f7
Onika Chorba <onikachorba@MacBook-Pro-Dream-2.local>
2022-12-18 16:32:16 +0200

    add filter popular and unpopular lessons

--- src/Pages/Find/components/FilterSelect/FilterSelect.jsx
+++ src/Pages/Find/components/FilterSelect/FilterSelect.jsx
@@ -1,33 +1,33 @@
 import "./FilterSelect.scss";
-import React from "react";
+import React, { useState } from "react";
 
-class FilterSelect extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: "all" };
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
+// class FilterSelect extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { value: "all" };
+//     this.handleChange = this.handleChange.bind(this);
+//   }
+//   handleChange(event) {
+//     this.setState({ value: event.target.value });
+//   }
 
-  render() {
-    return (
-      <div className="filterSelect">
-        <select value={this.state.value} onChange={this.handleChange}>
-          <option value="all" className="selectOption">
-            All Lessons
-          </option>
-          <option value="high" className="selectOption">
-            Popular Lessons
-          </option>
-          <option value="low" className="selectOption">
-            Unpopular Lessons
-          </option>
-        </select>
-      </div>
-    );
-  }
+// render() {
+function FilterSelect() {
+  return (
+    <div className="filterSelect">
+      <select>
+        <option value="all" className="selectOption">
+          All Lessons
+        </option>
+        <option value="high" className="selectOption">
+          Popular Lessons
+        </option>
+        <option value="low" className="selectOption">
+          Unpopular Lessons
+        </option>
+      </select>
+    </div>
+  );
 }
 
 export default FilterSelect;
